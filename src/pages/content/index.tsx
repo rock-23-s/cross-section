2f40f7943e37ec6cec11221e7866763d12d0341a
sunmy <281015408@qq.com>
2024-03-17 16:30:26 +0800

    feat: 增加项目总结

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,22 +1,9 @@
-// import { IconList, IconFont } from "@/icons/index"
 import styles from './index.less'
-import { union } from 'lodash'
 
 const Content: React.FC = () => {
   return <>
     <div className={styles.siderContent}>
       main content
-      {/* {
-        union(...IconList).map((item: {name: string}) => {
-          return (
-            <IconFont
-              name={item.name}
-              width="23"
-              key={item.name}
-            />
-          )
-        })
-      } */}
     </div>
   </>
 }
